d5dd9a0faac34db4cf5ab3a70c717c2587466f68
yyuneko <sarahlee11111122@gmail.com>
2022-11-20 22:07:27 +0800

    feat: add cookie functions

--- src/http/cookie.ts
+++ src/http/cookie.ts
@@ -1,54 +1,50 @@
 /**
  * 操作cookie，包含写入，读取，删除
  */
-function set(name: string, value: any, param: any) {
-	let savedValue = "";
-	if (typeof value === "object") {
-		savedValue = JSON.stringify(value);
-	} else {
-		savedValue = value;
-	}
-	const cookie = [
-		name,
-		"=",
-		encodeURIComponent(savedValue),
-		"; domain=",
-		param?.domain,
-		"; path=/;",
-	].join("");
-	document.cookie = cookie;
+function set(name: string, value: any) {
+  let savedValue: string;
+  if (typeof value === "object") {
+    savedValue = JSON.stringify(value);
+  } else {
+    savedValue = value;
+  }
+  document.cookie = [
+    name,
+    "=",
+    encodeURIComponent(savedValue),
+    "; path=/;",
+  ].join("");
 }
 
 function get(name: string) {
-	let result: any = document.cookie.match(new RegExp(`${name}=([^;]+)`));
-	result = result != null ? result[1] : "";
-	return decodeURIComponent(result);
+  let result: any = document.cookie.match(new RegExp(`${name}=([^;]+)`));
+  result = result != null ? result[1] : "";
+  return decodeURIComponent(result);
 }
 
-function remove(name: any, param: { domain: any }) {
-	document.cookie = [
-		name,
-		"=; expires=Thu, 01-Jan-1970 00:00:01 GMT; path=/; domain=",
-		param?.domain || "",
-	].join("");
+function remove(name: any) {
+  document.cookie = [
+    name,
+    "=; expires=Thu, 01-Jan-1970 00:00:01 GMT; path=/;",
+  ].join("");
 }
 
-function clearCookie(param: { domain: string }) {
-	const date = new Date();
-	date.setTime(date.getTime() - 10000);
-	const keys = document.cookie.match(/[^ =;]+(?=\\=)/g);
-	if (keys) {
-		for (let i = keys.length; i--; ) {
-			document.cookie =
-				`${keys[i]}=; expires=Thu, 01-Jan-1970 00:00:01 GMT` +
-					`; path=/;domain=${param?.domain}` || "";
-		}
-	}
+function clearCookie() {
+  const date = new Date();
+  date.setTime(date.getTime() - 10000);
+  const keys = document.cookie.match(/[^ =;]+(?=\\=)/g);
+  if (keys) {
+    for (let i = keys.length; i--; ) {
+      document.cookie =
+        `${keys[i]}=; expires=Thu, 01-Jan-1970 00:00:01 GMT` + `; path=/;` ||
+        "";
+    }
+  }
 }
 
 export default {
-	set,
-	get,
-	remove,
-	clearCookie,
+  set,
+  get,
+  remove,
+  clearCookie,
 };
