10c7a508bc21091bb58bf7d7b21ab0bb7ea1d3ef
yyuneko <sarahlee11111122@gmail.com>
2022-11-20 22:08:29 +0800

    feat: create axios instance

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,41 +1,57 @@
+import { refreshToken } from "@service/index";
 import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 import cookie from "./cookie";
 const axiosInstance = axios.create({
-	baseURL: "https://api.spotify.com/v1",
+  baseURL: "https://api.spotify.com/v1",
 });
 // axiosInstance.defaults.withCredentials = true;
-axiosInstance.defaults.headers.common["authorization"] = decodeURIComponent(
-	cookie.get("Authorization")
-);
+axiosInstance.interceptors.request.use((config) => {
+  if (config.baseURL === "http://localhost:8888") return config;
+  return new Promise<string>((resolve) => {
+    if (!cookie.get("Authorization")) {
+      refreshToken().then(() => {
+        resolve(cookie.get("Authorization"));
+      });
+    } else {
+      resolve(cookie.get("Authorization"));
+    }
+  }).then((res) => {
+    if (config) {
+      config.headers = config.headers || {};
+      config.headers.authorization = decodeURIComponent(res);
+    }
+    return config;
+  });
+});
 function get<T>(
-	url: string,
-	config?: AxiosRequestConfig
+  url: string,
+  config?: AxiosRequestConfig
 ): Promise<AxiosResponse<T>> {
-	return axiosInstance.get(url, config);
+  return axiosInstance.get(url, config) as Promise<AxiosResponse<T>>;
 }
 function post<T>(
-	url: string,
-	data?: any,
-	config?: AxiosRequestConfig
+  url: string,
+  data?: any,
+  config?: AxiosRequestConfig
 ): Promise<AxiosResponse<T>> {
-	return axiosInstance.post(url, data, config);
+  return axiosInstance.post(url, data, config) as Promise<AxiosResponse<T>>;
 }
 function put<T>(
-	url: string,
-	data?: any,
-	config?: AxiosRequestConfig
+  url: string,
+  data?: any,
+  config?: AxiosRequestConfig
 ): Promise<AxiosResponse<T>> {
-	return axiosInstance.put(url, data, config);
+  return axiosInstance.put(url, data, config) as Promise<AxiosResponse<T>>;
 }
 function del<T>(
-	url: string,
-	config?: AxiosRequestConfig
+  url: string,
+  config?: AxiosRequestConfig
 ): Promise<AxiosResponse<T>> {
-	return axiosInstance.delete(url, config);
+  return axiosInstance.delete(url, config) as Promise<AxiosResponse<T>>;
 }
 export default {
-	get,
-	post,
-	put,
-	del,
+  get,
+  post,
+  put,
+  del,
 };
