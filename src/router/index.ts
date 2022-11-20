6a46c2d4f1fd1bc3a9b6c6aee5e4013a2f68a56a
yyuneko <sarahlee11111122@gmail.com>
2022-11-20 22:10:38 +0800

    feat: define routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,33 +1,38 @@
 import { lazy } from "react";
 const AlbumDetail = lazy(() => import("@containers/AlbumDetail"));
 const PlayListDetail = lazy(() => import("@containers/PlayListDetail"));
+const FavoriteTracks = lazy(() => import("@containers/Favorite"));
 const Profile = lazy(() => import("@containers/Profile"));
 const Search = lazy(() => import("@containers/Search"));
 const Artist = lazy(() => import("@containers/Artist"));
 const routes = [
-	{
-		path: "album/:id",
-		component: AlbumDetail,
-	},
-	{
-		path: "playlist/:id",
-		component: PlayListDetail,
-	},
-	{
-		path: "seach/:keyword",
-		component: Search,
-	},
-	{
-		path: "user/:id",
-		component: Profile,
-	},
-	{
-		path: "favorite",
-		component: PlayListDetail,
-	},
-	{
-		path: "artist/:id",
-		component: Artist,
-	},
+  {
+    path: "album/:id",
+    component: AlbumDetail,
+  },
+  {
+    path: "playlist/:id",
+    component: PlayListDetail,
+  },
+  {
+    path: "search/:keyword",
+    component: Search,
+  },
+  {
+    path: "search",
+    component: Search,
+  },
+  {
+    path: "user/:id",
+    component: Profile,
+  },
+  {
+    path: "favorite",
+    component: FavoriteTracks,
+  },
+  {
+    path: "artist/:id",
+    component: Artist,
+  },
 ];
 export default routes;
