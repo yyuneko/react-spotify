4a43baa3c58197799e597a9a116a344c5322594e
yyuneko <sarahlee11111122@gmail.com>
2022-12-02 14:38:30 +0800

    feat: use InfiniteScroll in Table

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,5 +1,9 @@
-import React, { HTMLAttributes, ReactElement, TdHTMLAttributes } from "react";
 import classnames from "classnames";
+import React, { HTMLAttributes, ReactElement, TdHTMLAttributes } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+
+import { getValueWithKeys } from "@utils/index";
+
 import styles from "./index.module.less";
 
 export interface ColumnProp<RowType> {
@@ -19,8 +23,12 @@ interface TableProps<RowType> {
     row: RowType,
     index?: number
   ) => HTMLAttributes<any> | TdHTMLAttributes<any>;
-  getRowKey: any;
-  rowSelection: number[] | number;
+  rowKey: string | string[];
+  rowSelection?: number[] | number;
+  colcount: number;
+  disabledKey?: string | string[];
+  total: number;
+  next: () => void;
 }
 
 function Table<RowType = { [k: string]: any }>(props: TableProps<RowType>) {
@@ -29,53 +37,92 @@ function Table<RowType = { [k: string]: any }>(props: TableProps<RowType>) {
     dataSource = [],
     columns = [],
     onRow = () => ({}),
-    getRowKey,
+    rowKey,
     rowSelection,
+    colcount,
+    disabledKey,
+    total,
+    next,
   } = props;
+
   return (
-    <div className={styles["table__container"] + " " + className}>
-      <table>
-        <thead className={styles["table__thead"]}>
-          <tr className={styles["table__row"]}>
-            {columns.map((col) => (
-              <th
-                className={`text-align-${col.align ?? "left"}`}
-                style={col.width ? { width: col.width } : { maxWidth: "100%" }}
-              >
-                {col.title}
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody className={styles["table__tbody"]}>
-          {dataSource.map((item, index) => (
-            <tr
+    <div
+      className={styles["table__container"] + " " + className}
+      role="grid"
+      aria-colcount={colcount}
+    >
+      <div className={styles["table__thead"]} role="presentation">
+        <div className={styles["table__row"]} role="row" aria-rowindex="1">
+          {columns.map((col, colIndex) => 
+            <div
+              role="columnheader"
+              key={col.title}
+              aria-colindex={colIndex + 1}
               className={classnames({
+                [styles["table__cell"]]: true,
+                [`text-align-${col.align ?? "left"}`]: true,
+                [styles["table__cell-can-hidden"]]: [2, 3].includes(colIndex),
+              })}
+              style={col.width ? { width: col.width } : { maxWidth: "100%" }}
+            >
+              {col.title}
+            </div>
+          )}
+        </div>
+      </div>
+      <div role="presentation">
+        <InfiniteScroll
+          next={next}
+          loader={"loading"}
+          scrollableTarget="app__main"
+          dataLength={dataSource?.length}
+          hasMore={dataSource?.length < total}
+        >
+          {dataSource?.map?.((item, index) => 
+            <div
+              draggable
+              role="row"
+              {...onRow(item, index)}
+              aria-rowindex={index + 2}
+              key={getValueWithKeys(item, rowKey)}
+              aria-selected={
+                Array.isArray(rowSelection) && rowSelection.includes(index) ||
+                rowSelection === index
+              }
+              className={classnames({
+                [styles["table__rowDisabled"]]: !getValueWithKeys(
+                  item,
+                  disabledKey
+                ),
                 [styles["table__row"]]: true,
                 [styles["table__row-selected"]]:
-                  (Array.isArray(rowSelection) &&
-                    rowSelection.includes(index)) ||
+                  Array.isArray(rowSelection) &&
+                    rowSelection.includes(index) ||
                   rowSelection === index,
               })}
-              key={
-                typeof getRowKey === "function" ? getRowKey(item) : getRowKey
-              }
-              {...onRow(item)}
             >
-              {columns.map((col) => (
-                <td
-                  className={styles["table__cell"]}
+              {columns.map((col, colIndex) => 
+                <div
+                  role="gridcell"
+                  aria-colindex={colIndex + 1}
+                  key={getValueWithKeys(item, rowKey) + colIndex}
+                  className={classnames({
+                    [styles["table__cell"]]: true,
+                    [styles["table__cell-can-hidden"]]: [2, 3].includes(
+                      colIndex
+                    ),
+                  })}
                   style={
                     col.width ? { width: col.width } : { maxWidth: "100%" }
                   }
                 >
                   {col.render(item[col.dataIndex], item, index)}
-                </td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
+                </div>
+              )}
+            </div>
+          )}
+        </InfiniteScroll>
+      </div>
     </div>
   );
 }
