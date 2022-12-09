b66cf4c391066e86a8c38de23faeb31272e9b781
yyuneko <sarahlee11111122@gmail.com>
2022-12-09 14:38:14 +0800

    feat: add config about `less`

--- src/typings/assets.d.ts
+++ src/typings/assets.d.ts
@@ -1,22 +1,26 @@
 type SvgComponent = React.StatelessComponent<React.SVGAttributes<SVGElement>>;
-declare module '*.svg' {
+declare module "*.svg" {
   const content: SvgComponent;
   export default content;
 }
 
-declare module '*.png' {
+declare module "*.png" {
   const content: any;
   export default content;
 }
-declare module '*.jpg' {
+declare module "*.jpg" {
   const content: any;
   export default content;
 }
-declare module '*.jpeg' {
+declare module "*.jpeg" {
   const content: any;
   export default content;
 }
-declare module '*.gif' {
+declare module "*.gif" {
   const content: any;
   export default content;
+}
+declare module "*.less" {
+  const resource: { [key: string]: string };
+  export = resource;
 }
