0f3f6bd6d4f0afc3d9ddd186ca723153f4d426b0
yyuneko <sarahlee11111122@gmail.com>
2022-12-09 21:36:24 +0800

    feat: add recommendations in playlist detail page

--- src/containers/PlayListDetail/index.tsx
+++ src/containers/PlayListDetail/index.tsx
@@ -8,13 +8,19 @@ import { FollowPlaylist } from "@components/Follow";
 import Image from "@components/Image";
 import Join from "@components/Join";
 import Link from "@components/Link";
+import Loading from "@components/Loading";
 import { PlaylistMenu } from "@components/Menu";
 import NavBar from "@components/NavBar";
 import PlayButton from "@components/PlayButton";
 import Table from "@components/Table";
-import { getPlaylist, getPlaylistsTracks } from "@service/playlists";
+import {
+  addTracksToPlaylist,
+  getPlaylist,
+  getPlaylistsTracks,
+} from "@service/playlists";
 import { PlaylistTrackObject } from "@service/playlists/types";
-import { TrackObject } from "@service/tracks/types";
+import { getRecommendations } from "@service/tracks";
+import { SimplifiedTrackObject, TrackObject } from "@service/tracks/types";
 import { state } from "@store/index";
 import { setTitle } from "@store/ui/reducer";
 import useColumns from "@utils/columns";
@@ -23,8 +29,7 @@ import { useSpotifyPlayer } from "@utils/player";
 
 import styles from "./index.module.less";
 
-function PlayListDetail(props: any) {
-  const { className } = props;
+function PlayListDetail() {
   const { id } = useParams();
   const user = useCurrentUser();
   const spotify = useSpotifyPlayer();
@@ -37,7 +42,7 @@ function PlayListDetail(props: any) {
     dayjs.duration(
       tracks.length
         ? tracks
-          .map((item) => item.track.duration_ms)
+          .map((item) => item.track?.duration_ms ?? 0)
           .reduce((sum, current) => sum + current)
         : 0
     )
@@ -55,19 +60,24 @@ function PlayListDetail(props: any) {
     (state) => state.player.trackWindow.currentTrack
   );
 
-  const { data: playlistDetail, run: runGetPlaylistDetail } = useRequest(
-    getPlaylist,
-    {
-      manual: true,
-      onSuccess: (res) => {
-        res && setTracks(res.tracks.items);
-      },
-    }
-  );
+  const {
+    data: playlistDetail,
+    run: runGetPlaylistDetail,
+    loading: initialLoading,
+  } = useRequest(getPlaylist, {
+    manual: true,
+    onSuccess: (res) => {
+      res && setTracks(res.tracks.items);
+    },
+  });
   const columns = useColumns({
     contextUri: playlistDetail?.uri,
     album: { visible: (colCnt) => colCnt >= 4 },
     addAt: { show: true, visible: (colCnt: number) => colCnt >= 5 },
+  });
+  const extenderColumns = useColumns({
+    number: { show: false },
+    operations: { show: false },
   });
   const { run: runGetPlaylistTracks, loading: runGetPlaylistTracksLoading } =
     useRequest(getPlaylistsTracks, {
@@ -76,7 +86,23 @@ function PlayListDetail(props: any) {
         res && setTracks(tracks.concat(res.items));
       },
     });
+  const { data: recommendationTracks, run: runGetRecommendationTracks } =
+    useRequest(getRecommendations, { manual: true });
+  const { run: runAddTracksToPlaylist } = useRequest(addTracksToPlaylist, {
+    manual: true,
+    onSuccess: () => {
+      if (runGetPlaylistTracksLoading) {
+        return;
+      }
 
+      id &&
+        runGetPlaylistTracks(id, {
+          offset: tracks.length,
+          limit: 20,
+          additional_types: "track",
+        });
+    },
+  });
   const followersCount = useMemo(
     () =>
       playlistDetail &&
@@ -166,97 +192,165 @@ function PlayListDetail(props: any) {
     },
   });
 
-  return playlistDetail ? 
-    <>
-      <NavBar />
-      <div
-        style={{
-          display: "flex",
-          width: "100%",
-          alignItems: "flex-end",
-          boxSizing: "border-box",
-        }}
-        className="pl-16 pr-16 pb-24"
-      >
-        <Image
-          alt="cover"
-          className={styles["playlistCover"]}
-          src={playlistDetail.images?.[0]?.url}
-        />
-        <div style={{ flex: "1" }}>
-          <span className="text-base">{formatMessage({ id: "playlist" })}</span>
-          <div className="text-xxl text-base text-bold">
-            {playlistDetail.name}
-          </div>
-          <p className="text-m">{playlistDetail.description}</p>
-          <div className="inline-flex" style={{ alignItems: "baseline" }}>
-            <Join type="dot">
-              <Link
-                to={`/user/${playlistDetail.owner.id}`}
-                className="text-base text-bold"
-              >
-                {playlistDetail.owner.display_name}
-              </Link>
+  const handleAddTracksToCurrentPlaylist = (ids: string[]) => {
+    id && runAddTracksToPlaylist(id, { uris: ids.join(",") });
+  };
 
-              {!!playlistDetail.followers.total && 
-                <span className="text-base">{followersCount}</span>
-              }
+  const randomPick = (array: any[], count = 5) => {
+    if (array.length <= count) {
+      return array;
+    }
 
-              <span className="text-base">{tracksCount}</span>
-            </Join>
-            ,&nbsp;
-            <span>{duration}</span>
+    const result = [];
+
+    for (let i = 0; i < count; ++i) {
+      const idx = Math.floor(Math.random() * array.length);
+      result.push(array[idx]);
+    }
+
+    return result;
+  };
+
+  const handleGetRecommendations = () => {
+    const trackIds = Array.from(
+      new Set(
+        tracks.filter((track) => track.track).map((track) => track.track.id)
+      )
+    );
+    runGetRecommendationTracks({
+      seed_artists: "",
+      seed_genres: "",
+      seed_tracks: randomPick(trackIds, 5).join(","),
+    });
+  };
+
+  return (
+    <Loading loading={initialLoading}>
+      {playlistDetail && 
+        <>
+          <NavBar />
+          <div
+            style={{
+              display: "flex",
+              width: "100%",
+              alignItems: "flex-end",
+              boxSizing: "border-box",
+            }}
+            className="pl-16 pr-16 pb-24"
+          >
+            <Image
+              alt="cover"
+              className={styles["playlistCover"]}
+              src={playlistDetail.images?.[0]?.url}
+            />
+            <div style={{ flex: "1" }}>
+              <span className="text-base">
+                {formatMessage({ id: "playlist" })}
+              </span>
+              <div className="text-xxl text-base text-bold">
+                {playlistDetail.name}
+              </div>
+              <p className="text-m">{playlistDetail.description}</p>
+              <div className="inline-flex" style={{ alignItems: "baseline" }}>
+                <Join type="dot">
+                  <Link
+                    to={`/user/${playlistDetail.owner.id}`}
+                    className="text-base text-bold"
+                  >
+                    {playlistDetail.owner.display_name}
+                  </Link>
+
+                  {!!playlistDetail.followers.total && 
+                    <span className="text-base">{followersCount}</span>
+                  }
+
+                  <span className="text-base">{tracksCount}</span>
+                </Join>
+                ,&nbsp;
+                <span>{duration}</span>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      <div
-        style={{ height: "104px", boxSizing: "border-box" }}
-        className="flex p-24"
-      >
-        <PlayButton
-          size={56}
-          className="mr-32"
-          isPlaying={!paused && context.uri === playlistDetail.uri}
-          onClick={handlePlayCurrentPlaylist}
-        />
-        {user && playlistDetail && playlistDetail.owner.id != user?.id && 
-          <FollowPlaylist id={id} className="mr-24" />
-        }
-        <PlaylistMenu id={id} />
-      </div>
-      <div className="pl-16 pr-16">
-        <Table<Omit<PlaylistTrackObject, "track"> & TrackObject>
-          gridTemplateColumns={{
-            5: "16px 6fr 4fr 3fr minmax(120px, 1fr)",
-            4: "16px 4fr 2fr minmax(120px, 1fr)",
-            3: "16px 4fr minmax(120px, 1fr)",
-          }}
-          colcount={colcount >= 5 ? 5 : colcount >= 4 ? 4 : 3}
-          total={playlistDetail.tracks.total}
-          next={handleSearchTracks}
-          dataSource={tracks.map((item) => ({
-            ...item.track,
-            is_saved: item.is_saved,
-            added_at: item.added_at,
-            added_by: item.added_by,
-          }))}
-          columns={columns}
-          rowKey={(item) => `${item.id}_${item.album.id}`}
-          enabledKey="is_playable"
-          onRow={handleOnRow}
-          rowSelection={rowSelected}
-        />
-        <section>
-          <h1 className="text-base">
-            {formatMessage({ id: "playlist.extender.recommended.title" })}
-          </h1>
-          <p>{formatMessage({ id: "playlist.extender.songs.in.playlist" })}</p>
-        </section>
-      </div>
-    </>
-    : 
-    <div className={className}>Loading</div>
-  ;
+          <div
+            style={{ height: "104px", boxSizing: "border-box" }}
+            className="flex p-24"
+          >
+            <PlayButton
+              size={56}
+              className="mr-32"
+              isPlaying={!paused && context.uri === playlistDetail.uri}
+              onClick={handlePlayCurrentPlaylist}
+            />
+            {user && playlistDetail.owner.id != user?.id && 
+              <FollowPlaylist id={id} className="mr-24" />
+            }
+            <PlaylistMenu id={id} />
+          </div>
+          <div className="pl-16 pr-16">
+            <Table<Omit<PlaylistTrackObject, "track"> & TrackObject>
+              gridTemplateColumns={{
+                5: "16px 6fr 4fr 3fr minmax(120px, 1fr)",
+                4: "16px 4fr 2fr minmax(120px, 1fr)",
+                3: "16px 4fr minmax(120px, 1fr)",
+              }}
+              colcount={colcount >= 5 ? 5 : colcount >= 4 ? 4 : 3}
+              total={playlistDetail.tracks.total}
+              next={handleSearchTracks}
+              dataSource={tracks.map((item) => ({
+                ...item.track,
+                is_saved: item.is_saved,
+                added_at: item.added_at,
+                added_by: item.added_by,
+              }))}
+              columns={columns}
+              rowKey={(item) => `${item.id}_${item.album.id}`}
+              enabledKey="is_playable"
+              onRow={handleOnRow}
+              rowSelection={rowSelected}
+            />
+            {user?.id === playlistDetail.owner.id && 
+              <section>
+                <h1 className="text-base">
+                  {formatMessage({ id: "playlist.extender.recommended.title" })}
+                </h1>
+                <p>
+                  {formatMessage({ id: "playlist.extender.songs.in.playlist" })}
+                </p>
+                <Table<SimplifiedTrackObject>
+                  showHeader={false}
+                  dataSource={recommendationTracks?.tracks ?? []}
+                  colcount={3}
+                  columns={extenderColumns.concat([
+                    {
+                      dataIndex: "uri",
+                      title: "addToPlaylist",
+                      visible: true,
+                      align: "right",
+                      render: (t) => 
+                        <button
+                          onClick={() => handleAddTracksToCurrentPlaylist([t])}
+                        >
+                          {formatMessage({
+                            id: tracks.find((track) => track.track.uri === t)
+                              ? "duplicate.tracks.alreadyAdded"
+                              : "playlist.extender.button.add",
+                          })}
+                        </button>
+                      ,
+                    },
+                  ])}
+                  rowKey={(row) => `${row.id}_${row.album?.id}`}
+                  total={recommendationTracks?.tracks?.length ?? 1}
+                  next={handleGetRecommendations}
+                  gridTemplateColumns="4fr 2fr minmax(120px, 1fr)"
+                />
+              </section>
+            }
+          </div>
+        </>
+      }
+    </Loading>
+  );
 }
 
 export default PlayListDetail;
