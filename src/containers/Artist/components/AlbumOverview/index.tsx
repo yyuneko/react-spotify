8c24eee0b3ed72bebcc4e60043eb76cd8f69f9c4
yyuneko <sarahlee11111122@gmail.com>
2022-12-17 15:42:11 +0800

    feat: add discography page

---
+++ src/containers/Artist/components/AlbumOverview/index.tsx
@@ -0,0 +1,225 @@
+import { useRequest } from "ahooks";
+import React, { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
+import { useIntl } from "react-intl";
+import { useSelector } from "react-redux";
+
+import Paused from "@assets/icons/paused.svg";
+import Playing from "@assets/icons/playing.svg";
+import { Follow } from "@components/Follow";
+import Image from "@components/Image";
+import Join from "@components/Join";
+import Link from "@components/Link";
+import Table from "@components/Table";
+import { getAnAlbumsTracks } from "@service/albums";
+import { SimplifiedAlbumObject } from "@service/albums/types";
+import { checkUsersSavedTracks, removeTracksUser, saveTracksUser } from "@service/tracks";
+import { SimplifiedTrackObject, TrackObject } from "@service/tracks/types";
+import { state } from "@store/index";
+import useColumns from "@utils/columns";
+import { dayjs, format, usePlayContext } from "@utils/index";
+import { useSpotifyPlayer } from "@utils/player";
+
+import { CommonProps } from "../../../../type";
+import styles from "../../index.module.less";
+
+interface AlbumOverviewProps extends CommonProps {
+  id: string;
+  albumDetail: SimplifiedAlbumObject;
+  setCurrentAlbum: (albumDetail?: SimplifiedAlbumObject) => void;
+  currentAlbum?:SimplifiedAlbumObject;
+}
+
+export default function AlbumOverview(props: AlbumOverviewProps) {
+  const { id, albumDetail, setCurrentAlbum,currentAlbum } = props;
+  const { formatMessage } = useIntl();
+  const { ref: headerRef, inView: headerInview } = useInView({
+    root: document.querySelector("#app__main"),
+    threshold: 0.2,
+    // triggerOnce: true,
+  });
+  const { ref: bodyRef, inView: bodyInview } = useInView({
+    root: document.querySelector("#app__main"),
+    threshold: 0.2,
+    // triggerOnce: true,
+  });
+  const spotify = useSpotifyPlayer();
+  const paused = useSelector<state, boolean>((state) => state.player.paused);
+  const currentTrack = useSelector<state, TrackObject | undefined>(
+    (state) => state.player.trackWindow.currentTrack
+  );
+  const currentDevice = useSelector<state, string | undefined>(
+    (state) => state.player.device.current
+  );
+  const context = useSelector<
+    state,
+    { type?: string; id?: string; uri?: string }
+  >((state) => state.player.context);
+  const [tracks, setTracks] = useState<SimplifiedTrackObject[]>([]);
+  const [rowSelected, setRowSelected] = useState<number | undefined>();
+  const columns = useColumns({
+    image: { show: false },
+    contextUri: albumDetail?.uri,
+    album: { show: false },
+    addAt: { show: false },
+  });
+  const { run: runGetAlbumTracks, loading: runGetAlbumTracksLoading } =
+    useRequest(getAnAlbumsTracks, {
+      manual: true,
+      onSuccess: (res) => {
+        res && setTracks(tracks.concat(res.items));
+      },
+    });
+
+  const { data: isSavingTrack, run: checkIsSavingTrack } = useRequest(
+    checkUsersSavedTracks,
+    { manual: true }
+  );
+
+  const { run: runSaveTrack } = useRequest(saveTracksUser, {
+    manual: true,
+    onSuccess: () => {
+      currentTrack?.id && checkIsSavingTrack({ ids: currentTrack.id });
+    },
+  });
+
+  const { run: runRemoveTrack } = useRequest(removeTracksUser, {
+    manual: true,
+    onSuccess: () => {
+      currentTrack?.id && checkIsSavingTrack({ ids: currentTrack.id });
+    },
+  });
+
+  useEffect(() => {
+    if(!headerInview && bodyInview){setCurrentAlbum(albumDetail);}
+    else if(currentAlbum?.id === id){setCurrentAlbum(undefined);}
+
+    if (id && bodyInview && tracks.length === 0) {
+      setTracks([]);
+      runGetAlbumTracks(id, { limit: 20 });
+    }
+  }, [id, bodyInview,headerInview,albumDetail,currentAlbum]);
+
+  const handlePlayCurrentAlbum = usePlayContext({ 
+    type: "context",
+    uri: albumDetail.uri 
+  });
+
+  const handleSearchTracks = () => {
+    if (runGetAlbumTracksLoading) {
+      return;
+    }
+
+    id &&
+    runGetAlbumTracks(id, {
+      offset: tracks.length,
+      limit: 20,
+    });
+  };
+
+  const handleOnRow = (row: SimplifiedTrackObject, index: number) => ({
+    onClick: () => {
+      if (index === rowSelected) {
+        setRowSelected(undefined);
+      } else {
+        setRowSelected(index);
+      }
+    },
+    onDoubleClick: () => {
+      spotify.start(
+        { device_id: currentDevice },
+        {
+          context_uri: albumDetail?.uri,
+          offset: { position: index },
+        }
+      );
+    },
+  });
+
+  const toggleSaveTrack = () => {
+    if (currentTrack?.id) {
+      if (isSavingTrack?.[0]) {
+        runRemoveTrack({ ids: currentTrack.id });
+      } else {
+        runSaveTrack({ ids: currentTrack.id });
+      }
+    }
+  };
+
+  return <div className={styles["albumOverview"]}>
+    <div
+      className="p-32 flex w-full items-end box-border gap-24 items-start"
+      ref={headerRef}
+    >
+      <Image
+        alt="cover"
+        className="w-136 aspect-square"
+        src={albumDetail?.images?.[0]?.url}/>
+      <div className="flex-1 flex-col">
+        <Link
+          to={`/album/${albumDetail?.id}`}
+          className="text-4xl text-base py-16 font-black">
+          {albumDetail?.name}
+        </Link>
+        {albumDetail && <Join type="dot">
+          <span>
+            {formatMessage({ id: albumDetail?.album_type })}
+          </span>
+          <span>
+            {dayjs(albumDetail.release_date)
+              .year()}
+          </span>
+          <span>
+            {format(
+              formatMessage({
+                id: "tracklist-header.songs-counter",
+              }),
+              albumDetail.total_tracks
+            )}
+          </span>
+        </Join>}
+        <div className="inline-flex gap-16 pt-8 items-center">
+          {(paused || context.uri != albumDetail.uri) &&
+            <button
+              onClick={handlePlayCurrentAlbum}
+              className={styles["togglePlay"] + " button"}
+              title={formatMessage({ id: "playback-control.play" })}
+            >
+              <Paused/>
+            </button>
+          }
+          {!paused && context.uri === albumDetail.uri &&
+            <button
+              onClick={handlePlayCurrentAlbum}
+              className={styles["togglePlay"] + " button"}
+              title={formatMessage({ id: "playback-control.pause" })}
+            >
+              <Playing/>
+            </button>
+          }
+          <Follow
+            size={24}
+            onClick={toggleSaveTrack}
+            followed={isSavingTrack?.[0]}
+            style={{ width: "var(--button-size)" }}
+          />
+        </div>
+      </div>
+    </div>
+    <Table<SimplifiedTrackObject>
+      ref={bodyRef}
+      scrollableTarget={`album_${albumDetail.id}`}
+      className="px-16 xl:px-32"
+      gridTemplateColumns="16px 4fr minmax(120px, 1fr)"
+      colcount={3}
+      total={albumDetail.total_tracks}
+      next={handleSearchTracks}
+      dataSource={tracks}
+      columns={columns}
+      rowKey="id"
+      enabledKey="is_playable"
+      onRow={handleOnRow}
+      rowSelection={rowSelected}
+    />
+  </div>;
+}
---
+++ src/containers/Artist/components/Discography/index.tsx
@@ -0,0 +1,176 @@
+import { useRequest } from "ahooks";
+import classnames from "classnames";
+import React, { useEffect, useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { useInView } from "react-intersection-observer";
+import { useIntl } from "react-intl";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router-dom";
+
+import GridView from "@assets/icons/grid.svg";
+import ListView from "@assets/icons/list.svg";
+import { AlbumCard } from "@components/Card";
+import NavBar from "@components/NavBar";
+import PlayButton from "@components/PlayButton";
+import AlbumOverview from "@containers/Artist/components/AlbumOverview";
+import { SimplifiedAlbumObject } from "@service/albums/types";
+import { getAnArtist, getAnArtistsAlbums } from "@service/artists";
+import { state } from "@store/index";
+import { setTitle } from "@store/ui/reducer";
+import { dayjs, usePlayContext } from "@utils/index";
+
+import Link from "../../../../components/Link";
+
+export default function Discography() {
+  const { id, filter } = useParams();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { formatMessage } = useIntl();
+  const { ref, entry } = useInView({
+    root: document.querySelector("#app__main"),
+    threshold: [1]
+  });
+  const paused = useSelector<state, boolean>((state) => state.player.paused);
+  const colcount = useSelector<state, number>((state) => state.ui.colcount);
+  const context = useSelector<state,
+    { type?: string; id?: string; uri?: string }>((state) => state.player.context);
+  const [currentAlbum, setCurrentAlbum] = useState<SimplifiedAlbumObject>();
+  const [viewType, setViewType] = useState<"list" | "grid">("list");
+  const [total, setTotal] = useState(0);
+  const [albums, setAlbums] = useState<SimplifiedAlbumObject[]>([]);
+  const { data: artist, run: runGetArtistDetail } =
+    useRequest(getAnArtist, { manual: true });
+  const { run: runGetAlbums } = useRequest(getAnArtistsAlbums, {
+    manual: true,
+    onSuccess: res => {
+      setTotal(res.total);
+      setAlbums(albums.concat(res.items));
+    }
+  });
+  useEffect(() => {
+    if (id && filter) {
+      setAlbums([]);
+
+      if(filter === "all") {
+        runGetAlbums(id, {
+          include_groups: "album,single,appears_on,compilation",
+          limit: 20
+        });
+      }else{
+        runGetAlbums(id,{ include_groups: filter,limit: 20 });
+      }
+    }
+  }, [id, filter]);
+
+  useEffect(() => {
+    id && runGetArtistDetail(id);
+  }, [id]);
+  useEffect(() => {
+    if (artist && paused) {
+      dispatch(setTitle(
+        `Spotify - ${artist.name} - ${formatMessage({ id: "artist-page.discography" })}`
+      ));
+    }
+  }, [artist, paused]);
+
+  const handlePlayCurrentAlbum = usePlayContext({
+    type: "context",
+    uri: currentAlbum?.uri
+  });
+
+  const next = () => {
+    id && runGetAlbums(id, { limit: 20, offset: albums.length });
+  };
+
+  return <>
+    <NavBar>
+      <div
+        className={"flex gap-16 items-center opacity-0 transition-opacity " + classnames({
+          "duration-300 opacity-100": (entry?.intersectionRatio ?? 1) < 1 && currentAlbum
+        })}>
+        <PlayButton
+          isPlaying={!paused && context.uri === currentAlbum?.uri}
+          size={48}
+          onClick={handlePlayCurrentAlbum}/>
+        <h1 className="text-base text-2xl text-overflow-ellipsis">
+          {currentAlbum?.name}
+        </h1>
+      </div>
+    </NavBar>
+    <section>
+      <div ref={ref}/>
+      <div
+        className={"flex justify-between px-16 xl:px-32 sticky top-64 "
+          + classnames({ isPinned: (entry?.intersectionRatio ?? 1) < 1 })
+        }>
+        <Link
+          to={`/artist/${artist?.id}`}
+          className="text-base text-2xl text-bold h-40 m-0"
+        >
+          {artist?.name}
+        </Link>
+        <div className="flex gap-16 items-center">
+          <select onChange={e => navigate(`/artist/${id}/discography/${e.target.value}`)}>
+            <option value="all" defaultChecked={filter === "all"}>
+              {formatMessage({ id: "artist-page-discography.all" })}
+            </option>
+            <option value="album" defaultChecked={filter === "album"}>
+              {formatMessage({ id: "artist.albums" })}
+            </option>
+            <option value="single" defaultChecked={filter === "single"}>
+              {formatMessage({ id: "artist.singles" })}
+            </option>
+          </select>
+          <button 
+            className="button rounded-full w-32 h-32"
+            onClick={() => setViewType("list")}
+            style={{ 
+              backgroundColor: viewType === "list" ? "hsla(0, 0%, 100%, 0.1)" : undefined
+            }}
+          >
+            <ListView width="16" height="16"/>
+          </button>
+          <button
+            className="button rounded-full w-32 h-32"
+            onClick={() => setViewType("grid")}
+            style={{ 
+              backgroundColor: viewType === "grid" ? "hsla(0, 0%, 100%, 0.1)" : undefined
+            }}
+          >
+            <GridView width="16" height="16"/>
+          </button>
+        </div>
+      </div>
+      <InfiniteScroll
+        next={next}
+        hasMore={albums.length < total}
+        loader={"Loading"}
+        dataLength={albums.length}
+        scrollableTarget="app__main"
+      >
+        {viewType === "list" ? <div>
+          {albums.map(album => <AlbumOverview
+            key={album.id}
+            id={album.id}
+            albumDetail={album}
+            currentAlbum={currentAlbum}
+            setCurrentAlbum={setCurrentAlbum}
+          />)}
+        </div> :
+          <div
+            className="grid pl-16 pr-16 xl:pl-32 xl:pr-32"
+            aria-colcount={colcount}
+            style={{ gridTemplateColumns: "repeat(var(--col-count),1fr)" }}
+          >
+            {albums.map(album => <AlbumCard id={album.id} media={album.images[0]?.url}
+              name={album.name} yearOrArtists={<div>
+                <span>{dayjs(album.release_date)
+                  .year()}</span>
+                {" • "}
+                <span>{formatMessage({ id: album.album_type })}</span>
+              </div>}/>)}
+          </div>}
+      </InfiniteScroll>
+    </section>
+  </>;
+}
