ff1c6280525f53a7bb3ec8cf609b298c758ccea5
yyuneko <sarahlee11111122@gmail.com>
2022-12-17 13:09:13 +0800

    fix: prevent bubbling

--- src/components/Link/index.tsx
+++ src/components/Link/index.tsx
@@ -4,19 +4,21 @@ import { Link as RouterLink, LinkProps } from "react-router-dom";
 import "./index.less";
 import { CommonProps } from "../../type";
 
-interface Props extends CommonProps, LinkProps {
-  ellipsis?: boolean;
-}
+type Props = CommonProps & LinkProps
 
 function Link(props: Props) {
-  const { children, className, to, ...otherProps } = props;
+  const { children, className, to, onClick,...otherProps } = props;
 
   return (
     <RouterLink
       to={to}
-      {...otherProps}
       className={"link " + (className ?? "")}
       data-title={children}
+      onClick={(e) => {
+        onClick?.(e);
+        e.stopPropagation();
+      }}
+      {...otherProps}
     >
       {children}
     </RouterLink>
