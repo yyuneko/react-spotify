72dda9f1a00afaa392326b8cf53591193fef3840
yyuneko <sarahlee11111122@gmail.com>
2022-12-19 20:36:27 +0800

    feat: get dominated color of cover image in component `ContentContainer`

--- src/components/ContentContainer/index.tsx
+++ src/components/ContentContainer/index.tsx
@@ -1,5 +1,7 @@
 import classnames from "classnames";
-import React, { ReactNode, useMemo } from "react";
+import Vibrant from "node-vibrant";
+import { Palette } from "node-vibrant/lib/color";
+import React, { ReactNode, useEffect, useMemo, useState } from "react";
 import { useInView } from "react-intersection-observer";
 import { useIntl } from "react-intl";
 import { useSelector } from "react-redux";
@@ -41,97 +43,125 @@ export default function ContentContainer(props: ContentContainerProps) {
     description,
     contextUri,
     operationExtra,
+    className = ""
   } = props;
   const { formatMessage } = useIntl();
   const { ref, entry } = useInView({
     root: document.querySelector("#app__main"),
     threshold: 1
   });
+  const colcount = useSelector<state, number>((state) => state.ui.colcount);
   const paused = useSelector<state, boolean>((state) => state.player.paused);
   const context = useSelector<state,
     { type?: string; id?: string; uri?: string; name?: string }>
     ((state) => state.player.context);
   const fontSize = useSelector<state, number>(state => state.ui.fontSize);
   const handlePlayCurrentContext = usePlayContext({ uri: contextUri });
+  const [dominatedColor, setDominatedColor] = useState("var(--bg-base)");
 
-  const titleFontSize = useMemo( () => {
+  const titleFontSize = useMemo(() => {
     if (title) {
       const width = (document.getElementById("app__main")
         ?.getBoundingClientRect()?.width ?? 300) - 300;
-      console.log(width, fontSize);
-      const autoFitFontSize = width / title.length / fontSize;
+      const length = title.split("")
+        .reduce((sum, cur) =>
+          (cur.charCodeAt(0) <= 255 ? 0.5 : 1) + sum, 0) * fontSize;
 
-      if (autoFitFontSize < 2) {
-        return 2;
-      }
-
-      if (autoFitFontSize > 6) {
+      if (6 * length <= width) {
         return 6;
       }
 
-      if (autoFitFontSize >= 5) {
-        return 6;
-      }
-
-      if (autoFitFontSize >= 4) {
+      if (4.5 * length <= width) {
         return 4.5;
       }
 
-      if (autoFitFontSize >= 3) {
+      if (3.5 * length <= width) {
+        return 3.5;
+      }
+
+      if (3 * length <= width) {
         return 3;
       }
+
+      return 2;
     }
 
-    return 2;
-  },[title]);
+  }, [title, colcount]);
+  useEffect(() => {
+    if (cover) {
+      const img = Vibrant.from(cover);
+      img.getPalette()
+        .then((colors:Palette) => {
+          const all = Object.keys(colors)
+            .reduce((sum,cur) => sum + colors[cur]!.population,0);
+
+          if (colors.Vibrant && colors.Vibrant.population > all / 6) {
+            setDominatedColor(`rgb(${colors.Vibrant.rgb.join(",")})`);
+
+            return;
+          }
+
+          const items = Object.keys(colors)
+            .filter(key => colors[key]!.population > all / 6)
+            .sort((a,b) => colors[b]!.population - colors[a]!.population);
+          items[0] && setDominatedColor(`rgb(${colors[items[0]]!.rgb.join(",")})`);
+        });
+    }
+  }, [cover]);
 
   return (
     <Loading loading={initialLoading}>
-      <NavBar>
+      <div className="relative">
         <div
-          className={"flex gap-16 items-center opacity-0 transition-opacity "
-            + classnames({
-              "duration-300 opacity-100": (entry?.intersectionRatio ?? 1) < 1
-            })}>
-          <PlayButton
-            isPlaying={!paused && context.uri === contextUri}
-            size={48}
-            onClick={handlePlayCurrentContext}/>
-          <h1 className="text-base text-2xl text-overflow-ellipsis">
-            {title}
-          </h1>
-        </div>
-      </NavBar>
-      <div
-        className="pl-16 pr-16 xl:pl-32 xl:pr-32 pb-24 flex w-full items-end box-border"
-        style={{ maxHeight: "436px", minHeight: "276px", height: "calc(30vh - 64px)" }}
-      >
-        <Image
-          alt="cover"
-          fallback={fallback}
-          className={styles["cover"]} src={cover}
-          shape={type === "artist" ? "circle" : "square"}/>
-        <div className="flex-1">
-          {tag &&
-            <span className="text-base">{formatMessage({ id: tag })}</span>
-          }
+          style={{ backgroundColor: dominatedColor }}
+          className="w-full h-full absolute top-0 left-0"/>
+        <div className={"w-full h-full absolute top-0 left-0 " + styles["bg"]}/>
+        <NavBar>
           <div
-            style={title
-              ? { fontSize: `${titleFontSize}rem` }
-              : undefined}
-            className="text-8xl text-base py-16 font-black">{title}</div>
-          {description && <p className="text-m">{description}</p>}
-          {extra &&
-            <div className="inline-flex items-baseline">
-              {extra}
-            </div>
-          }
+            className={"flex gap-16 items-center opacity-0 transition-opacity "
+              + classnames({
+                "duration-300 opacity-100": (entry?.intersectionRatio ?? 1) < 1
+              })}>
+            <PlayButton
+              isPlaying={!paused && context.uri === contextUri}
+              size={48}
+              onClick={handlePlayCurrentContext}/>
+            <h1 className="text-base text-2xl text-overflow-ellipsis">
+              {title}
+            </h1>
+          </div>
+        </NavBar>
+        <div
+          className="px-16 xl:px-32 pb-24 flex w-full items-end box-border"
+          style={{ maxHeight: "436px", minHeight: "276px", height: "calc(30vh - 64px)" }}
+        >
+          <Image
+            alt="cover"
+            fallback={fallback}
+            className={styles["cover"] + " z-0"} src={cover}
+            shape={type === "artist" ? "circle" : "square"}/>
+          <div className="flex-1 z-0">
+            {tag &&
+              <span className="text-base">{formatMessage({ id: tag })}</span>
+            }
+            <div
+              style={title
+                ? { fontSize: `${titleFontSize}rem` }
+                : undefined}
+              className="text-8xl text-base py-16 font-black break-all">{title}</div>
+            {description && <p className="text-m">{description}</p>}
+            {extra &&
+              <div className="inline-flex items-baseline">
+                {extra}
+              </div>
+            }
+          </div>
         </div>
       </div>
       <div
         ref={ref}
         style={{ height: "104px" }}
-        className="flex pl-16 pr-16 xl:pl-32 xl:pr-32 pt-24 pb-24 box-border items-center"
+        className="flex px-16 xl:px-32 pt-24 pb-24 box-border items-center"
       >
         <PlayButton
           size={56}
@@ -141,7 +171,7 @@ export default function ContentContainer(props: ContentContainerProps) {
         />
         {operationExtra}
       </div>
-      <div>{children}</div>
+      <div className={className}>{children}</div>
     </Loading>
   );
 }
